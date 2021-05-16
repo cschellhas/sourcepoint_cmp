8973177b222f24a95a9c54dc7bf0c9406812a8b3
0phelia <artem.tghcd@gmail.com>
2021-05-16 20:59:41 +0200

    do not register plugin if FlutterView/FlutterEngine does not have an activity (like a homescreen android widget, for example)

--- android/src/main/kotlin/de/medienkontorfulda/sourcepoint_cmp/SourcepointCmpPlugin.kt
+++ android/src/main/kotlin/de/medienkontorfulda/sourcepoint_cmp/SourcepointCmpPlugin.kt
@@ -10,6 +10,11 @@ class SourcepointCmpPlugin {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
       val activity = registrar.activity()
+      if (activity == null) {
+        // When a background flutter view tries to register the plugin, the registrar has no activity.
+        // We stop the registration process as this plugin is foreground only.
+        return;
+      }
       val interstitialChannel =
               MethodChannel(registrar.messenger(), "sourcepoint_cmp")
       interstitialChannel.setMethodCallHandler(SourcepointCmp(registrar, interstitialChannel, activity))
